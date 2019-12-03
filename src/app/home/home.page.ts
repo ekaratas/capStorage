34fc096bf71e0f41cf36690d9bcd59ea83e63d9d
Erinc Karatas <1380275+ekaratas@users.noreply.github.com>
2019-12-04 01:24:27 +0300

    v1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  id;
+  name;
+
+  DataArray: Array<any> = [];
+
   constructor() {}
+
+  async setObject() {
+    await Storage.set({
+      key: 'bildirimler',
+      value: JSON.stringify(this.DataArray)
+    });
+  }
+
+  ekle() {
+
+    let deger = {
+      id : this.id,
+      name : this.name
+    }
+
+    this.DataArray.push(deger);
+    this.setObject();
+
+  }
+
+  async getObject() {
+    const ret = await Storage.get({ key: 'bildirimler' });
+    const liste = JSON.parse(ret.value);
+    console.log(liste);
+  }
 
 }
